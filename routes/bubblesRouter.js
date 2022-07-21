5e2918efbb87f26ef5d2de116e73445549865cb3
Franziska S <fra.se@posteo.net>
2022-07-21 21:44:03 +0200

    bubble router

--- routes/bubblesRouter.js
+++ routes/bubblesRouter.js
@@ -1,6 +1,22 @@
 import { Router } from "express";
 import { check } from "express-validator";
+import {
+  addBubble,
+  inviteMember,
+  deleteBubble,
+  findBubbleById,
+  listBubbles,
+  leaveBubble,
+  updateBubble,
+} from "../controllers/bubbleControllers.js";
+import { verifyAccessToken } from "../middlewares/verifyAccessToken.js";
 
-const bubblesRouter = Router();
-
-export default bubblesRouter;
+const bubbleRouter = Router();
+bubbleRouter.post("/", verifyAccessToken, addBubble);
+bubbleRouter.put("/:id", verifyAccessToken, updateBubble);
+bubbleRouter.delete("/:id", verifyAccessToken, deleteBubble);
+bubbleRouter.get("/", verifyAccessToken, listBubbles);
+bubbleRouter.get("/:id", verifyAccessToken, findBubbleById);
+bubbleRouter.put("/:bubbleId/invite", verifyAccessToken, inviteMember);
+bubbleRouter.put("/:bubbleId/leave", verifyAccessToken, leaveBubble);
+export default bubbleRouter;
