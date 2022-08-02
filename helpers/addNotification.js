4d3ab833ed0a227a4faeefdd0dab017e64b21287
Franziska S <fra.se@posteo.net>
2022-08-02 14:52:04 +0200

    check if notification already exists for user

--- helpers/addNotification.js
+++ helpers/addNotification.js
@@ -4,32 +4,46 @@ import Reco from "../models/Reco.js";
 
 export const addNotification = async (data) => {
   const { id, bubbleId, type, userIds, recoId } = data;
+
+  let filteredUserIds;
   if (type === "INVITATION_TO_BUBBLE") {
-    const bubbles = await Bubble.find({ members: id });
-    const bubbleIds = bubbles.map((bubble) => bubble._id);
     const bubbleNotifications = await Notification.find({
-      $and: [{ userIds: id }, { bubbleId: { $in: bubbleIds } }],
+      $and: [{ userIds: { $in: userIds } }, { bubbleId: bubbleId }],
     });
-    console.log(bubbleNotifications);
-    if (bubbleNotifications.length !== 0)
-      throw new Error("user is already invited to this bubble");
+    if (bubbleNotifications.length !== 0) {
+      const invitedUserIds = bubbleNotifications
+        .map((notification) => notification.userIds)
+        .flat();
+
+      filteredUserIds = userIds.filter((userId) => {
+        if (invitedUserIds.includes(userId)) return false;
+        return true;
+      });
+      if (filteredUserIds.length === 0) {
+        throw new Error("users are already invited to this bubble");
+      }
+    }
   }
   if (type === "INVITATION_TO_RECO") {
-    const recos = await Reco.find({ membersIds: id });
+    const recos = await Reco.find({ userIds: { $in: userIds } });
+    console.log({ recos });
     const recoIds = recos.map((reco) => reco._id);
+    console.log({ recoIds });
+    if (recoIds.includes(recoId)) throw new Error("user already has this reco");
     const recoNotifications = await Notification.find({
       $and: [{ userIds: id }, { recoId: { $in: recoIds } }],
     });
-    console.log(recoNotifications);
+
     if (recoNotifications.length !== 0)
       throw new Error("user is already invited to this reco");
   }
+
   try {
     const notification = await Notification.create({
       bubbleId,
       invitedBy: id,
       type,
-      userIds,
+      userIds: filteredUserIds || userIds,
       recoId,
     });
     return notification;
