8905da02f95122a2fb348ff7bd7ffac2bda6689e
Franziska S <fra.se@posteo.net>
2022-08-22 13:36:56 +0200

    added routes

--- routes/recoRouter.js
+++ routes/recoRouter.js
@@ -5,13 +5,17 @@ import {
   addReco,
   deleteReco,
   updateReco,
+  listBubbleRecos,
+  ignoreReco,
 } from "../controllers/recoController.js";
-import { verifyAccessToken } from "../middlewares/verifyAccessToken.js";
+import verifyAccessToken from "../middlewares/verifyAccessToken.js";
 
 const recoRouter = Router();
 recoRouter.get("/", verifyAccessToken, listRecos);
+recoRouter.get("/:id", verifyAccessToken, listBubbleRecos);
 recoRouter.post("/", verifyAccessToken, addReco);
 recoRouter.put("/:id", verifyAccessToken, updateReco);
+recoRouter.put("/:id/ignore", verifyAccessToken, ignoreReco);
 recoRouter.delete("/:id", verifyAccessToken, deleteReco);
 
 export default recoRouter;
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,15 +2,15 @@ import { Router } from "express";
 import { check } from "express-validator";
 import {
   findUserById,
-  updateUser,
+  unfriendUser,
   deleteUser,
 } from "../controllers/userController.js";
-import { verifyAccessToken } from "../middlewares/verifyAccessToken.js";
+import verifyAccessToken from "../middlewares/verifyAccessToken.js";
 
 const userRouter = Router();
 
 userRouter.get("/:id", verifyAccessToken, findUserById);
-userRouter.put("/:id", verifyAccessToken, updateUser);
+userRouter.put("/:id", verifyAccessToken, unfriendUser);
 userRouter.delete("/:id", verifyAccessToken, deleteUser);
 
 export default userRouter;
