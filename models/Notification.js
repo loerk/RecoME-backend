90c25be58656ea974a5234841b0dc8f143eb85b9
Franziska S <fra.se@posteo.net>
2022-08-22 13:35:25 +0200

    removed default population of notifications

--- models/Notification.js
+++ models/Notification.js
@@ -4,7 +4,7 @@ const { Schema, model } = mongoose;
 const notificationSchema = new Schema(
   {
     bubbleId: { type: String, ref: "Bubble", default: null },
-    invitedBy: { type: String, ref: "User" },
+    invitedBy: { type: String, ref: "User", default: null },
     type: String,
     userIds: { type: [String], ref: "User", default: null },
     recoId: { type: String, ref: "Reco", default: null },
