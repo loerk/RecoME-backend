08c45fe62e08e1d453f411da0cbf58607379c2cb
Franziska S <fra.se@posteo.net>
2022-08-22 13:33:40 +0200

    check if user is already invited and notified about invitation to bubble, create a filtered array of uninvited users

--- helpers/addNotification.js
+++ helpers/addNotification.js
@@ -1,29 +1,39 @@
-import Bubble from "../models/Bubble.js";
 import Notification from "../models/Notification.js";
 import Reco from "../models/Reco.js";
 
 export const addNotification = async (data) => {
-  const { id, bubbleId, type, userIds, recoId } = data;
-
+  const { _id, bubbleId, type, userIds, recoId } = data;
   let filteredUserIds;
+
   if (type === "INVITATION_TO_BUBBLE") {
+    // collect all notifications for this bubble
     const bubbleNotifications = await Notification.find({
-      $and: [{ userIds: { $in: userIds } }, { bubbleId: bubbleId }],
+      $and: [
+        { userIds: { $in: userIds } },
+        { bubbleId: bubbleId },
+        { type: "INVITATION_TO_BUBBLE" },
+      ],
     });
+
     if (bubbleNotifications.length !== 0) {
+      // get an array of all the invited userIds
       const invitedUserIds = bubbleNotifications
         .map((notification) => notification.userIds)
         .flat();
-
+      // filter the userIds to only include uninvited users
       filteredUserIds = userIds.filter((userId) => {
         if (invitedUserIds.includes(userId)) return false;
         return true;
       });
+
       if (filteredUserIds.length === 0) {
         throw new Error("users are already invited to this bubble");
       }
+    } else {
+      filteredUserIds = userIds;
     }
   }
+
   if (type === "INVITATION_TO_RECO") {
     const recos = await Reco.find({ userIds: { $in: userIds } });
     const recoIds = recos.map((reco) => reco._id);
