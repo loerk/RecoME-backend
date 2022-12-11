7d6392c8b0b75c480f25aa50aa9c242e4637acb2
Franziska S <fra.se@posteo.net>
2022-12-11 13:21:25 +0100

    debug email in prod

--- helpers/sendVerificationEmail.js
+++ helpers/sendVerificationEmail.js
@@ -1,15 +1,15 @@
-import sgMail from "@sendgrid/mail";
-import "dotenv/config";
+import sgMail from '@sendgrid/mail';
+import 'dotenv/config';
 
-import { emailHTML } from "./email.js";
+import { emailHTML } from './email.js';
 sgMail.setApiKey(process.env.REACT_APP_EMAIL_VERIFICATION_KEY);
 
 export const sendVerificationEmail = (token, userEmail, username) => {
   const msg = {
     to: userEmail,
-    from: "welcome-to-recome@posteo.de",
-    subject: "You signed up at recoMe",
-    text: "please verify your email",
+    from: 'welcome-to-recome@posteo.de',
+    subject: 'You signed up at recoMe',
+    text: 'please verify your email',
     html: emailHTML(token, username),
   };
 
@@ -21,7 +21,9 @@ export const sendVerificationEmail = (token, userEmail, username) => {
 
       if (error.response) {
         console.error(error.response.body);
+        return error.resonse.body;
       }
+      return error;
     }
   })();
 };
